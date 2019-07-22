8e4efa7612561e9a6860df3989eef56f0e257eaa
Logan Bomy <logan.bomy@promod.fr>
2019-07-22 14:43:18 +0200

    link download icons svg

--- pages/design-elements/social-icons.vue
+++ pages/design-elements/social-icons.vue
@@ -8,6 +8,14 @@
         <div class="code">
           <span class="punctuation">&lt;</span><span class="element">svg </span><span class="attr-name">class</span><span class="punctuation">=</span><span class="attr-value">"icon-svg"</span><span class="punctuation">&gt;</span><span class="punctuation">&lt;</span><span class="element">use </span><span class="attr-name">xlink:href</span><span class="punctuation">=</span><span class="attr-value">"#icon-facebook"</span><span class="punctuation">&gt;&lt;/</span><span class="element">use</span><span class="punctuation">&gt;&lt;/</span><span class="element">svg</span><span class="punctuation">&gt;</span>
         </div>
+        <div class="download">
+          <svg class="icon-svg"><use xlink:href="#icon-arrow" /></svg>
+          <a download="sprite.svg" href="../img/icons/sprite.svg">Download sprite.svg</a>
+        </div>
+        <div class="download">
+          <svg class="icon-svg"><use xlink:href="#icon-arrow" /></svg>
+          <a download="icons.zip" href="../img/icons/sources/icons.zip">Download icons.zip</a>
+        </div>
         <ul>
           <li>
             <svg class="icon-svg"><use xlink:href="#icon-facebook" /></svg>
